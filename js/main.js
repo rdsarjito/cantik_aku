d004476af57e99ccd96d14c19a33eb239cd91dfd
rdsarjito <rama.dhaninursarjito@gmail.com>
2024-12-05 17:42:29 +0700

    new 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('AKU SAYANG KAMU').split('')
+    const titles = ('FEBBY SEMANGAT YAA!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
